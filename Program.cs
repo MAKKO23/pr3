560754499654cd4e4fa7d962f6b32637dabe58f6
Columbia <info@columbia.com>
2024-05-11 09:14:57 +0400

    minus

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2Maxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            char zn;
+            Console.WriteLine("Введите первую переменную: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите вторую переменную: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите занк");
+            zn = Convert.ToChar(Console.ReadLine());
+            if (zn == '+')
+            {
+                c = a + b;
+                Console.WriteLine("Ответ: " + c);
+            }
+            else if (zn == '-')
+            {
+                c = a - b;
+                Console.WriteLine("Ответ: " + c);
+            }
+
+        }
+    }
+}
